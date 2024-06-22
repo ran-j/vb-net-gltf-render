6a0e09c929241eec8bd0180b004e0ea4ddc52d4b
Ran-j <ran-junior@hotmail.com>
2024-06-22 20:42:24 -0300

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        OpenglControl1 = New SharpGL.OpenGLControl()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        CType(OpenglControl1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' OpenglControl1
+        ' 
+        OpenglControl1.Dock = DockStyle.Fill
+        OpenglControl1.DrawFPS = True
+        OpenglControl1.FrameRate = 30
+        OpenglControl1.Location = New Point(0, 0)
+        OpenglControl1.Margin = New Padding(4, 3, 4, 3)
+        OpenglControl1.Name = "OpenglControl1"
+        OpenglControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1
+        OpenglControl1.RenderContextType = SharpGL.RenderContextType.DIBSection
+        OpenglControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased
+        OpenglControl1.Size = New Size(800, 450)
+        OpenglControl1.TabIndex = 0
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        TextBox1.Location = New Point(688, 362)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 1
+        TextBox1.Text = "10"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        TextBox2.Location = New Point(688, 391)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 2
+        TextBox2.Text = "10"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        TextBox3.Location = New Point(688, 420)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 3
+        TextBox3.Text = "10"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(OpenglControl1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(OpenglControl1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents OpenglControl1 As SharpGL.OpenGLControl
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,212 @@
+﻿Imports System.Numerics
+Imports SharpGL
+Imports SharpGL.SceneGraph
+Imports SharpGLTF.Schema2
+Imports System.Threading.Tasks
+Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.IO
+
+Public Class Form1
+    Private gltfModel As ModelRoot
+    Private textures As New Dictionary(Of Integer, UInteger)
+    Private enableAntialiasing As Boolean = False
+
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Await LoadGLTFModelAsync("F:\Old\Assets\lt\BoxTextured.glb")
+        ListMaterials()
+    End Sub
+
+    Private Async Function LoadGLTFModelAsync(filePath As String) As Task
+        gltfModel = Await Task.Run(Function() ModelRoot.Load(filePath))
+        LoadTextures()
+    End Function
+
+    Private Sub LoadTextures()
+        If gltfModel Is Nothing Then Return
+
+        Dim gl As OpenGL = OpenglControl1.OpenGL
+        gl.Enable(OpenGL.GL_TEXTURE_2D)
+
+        For Each texture In gltfModel.LogicalTextures
+            Dim bitmap = MemoryImageToBitmap(texture.PrimaryImage.Content)
+
+            Dim textureId As UInteger() = {0}
+            gl.GenTextures(1, textureId)
+            gl.BindTexture(OpenGL.GL_TEXTURE_2D, textureId(0))
+
+            Dim bitmapData = bitmap.LockBits(New Rectangle(0, 0, bitmap.Width, bitmap.Height), Imaging.ImageLockMode.ReadOnly, Imaging.PixelFormat.Format32bppArgb)
+            gl.PixelStore(OpenGL.GL_UNPACK_ALIGNMENT, 1)
+            gl.TexImage2D(OpenGL.GL_TEXTURE_2D, 0, OpenGL.GL_RGBA, bitmap.Width, bitmap.Height, 0, OpenGL.GL_BGRA, OpenGL.GL_UNSIGNED_BYTE, bitmapData.Scan0)
+            bitmap.UnlockBits(bitmapData)
+
+            gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MIN_FILTER, OpenGL.GL_LINEAR)
+            gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MAG_FILTER, OpenGL.GL_LINEAR)
+            gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_WRAP_S, OpenGL.GL_REPEAT)
+            gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_WRAP_T, OpenGL.GL_REPEAT)
+
+            textures(texture.LogicalIndex) = textureId(0)
+        Next
+    End Sub
+
+    Private Function MemoryImageToBitmap(memoryImage As SharpGLTF.Memory.MemoryImage) As Bitmap
+        Using ms As New MemoryStream(memoryImage.Content.ToArray())
+            Return New Bitmap(ms)
+        End Using
+    End Function
+
+    Private Sub ListMaterials()
+        If gltfModel Is Nothing Then Return
+
+        Dim materials = New HashSet(Of Material)()
+
+        For Each scene In gltfModel.LogicalScenes
+            For Each node In scene.VisualChildren
+                GatherMaterials(node, materials)
+            Next
+        Next
+
+        For Each material In materials
+            Console.WriteLine("Material found: " & material.Name)
+        Next
+    End Sub
+
+    Private Sub GatherMaterials(node As Node, materials As HashSet(Of Material))
+        If node.Mesh IsNot Nothing Then
+            For Each prim In node.Mesh.Primitives
+                If prim.Material IsNot Nothing Then
+                    materials.Add(prim.Material)
+                End If
+            Next
+        End If
+
+        For Each child In node.VisualChildren
+            GatherMaterials(child, materials)
+        Next
+    End Sub
+
+    Private Sub RenderGLTFModel(gl As OpenGL)
+        If gltfModel Is Nothing Then Return
+
+        For Each scene In gltfModel.LogicalScenes
+            For Each node In scene.VisualChildren
+                RenderNode(gl, node)
+            Next
+        Next
+    End Sub
+
+    Private Sub RenderNode(gl As OpenGL, node As Node)
+        gl.PushMatrix()
+
+        Dim transform = node.WorldMatrix
+        Dim matrixArray = TransformMatrix(transform)
+        gl.MultMatrix(matrixArray)
+
+        ' Render the mesh from node
+        If node.Mesh IsNot Nothing Then
+            RenderMesh(gl, node.Mesh)
+        End If
+
+        For Each child In node.VisualChildren
+            RenderNode(gl, child)
+        Next
+
+        gl.PopMatrix()
+    End Sub
+
+    Private Sub RenderMesh(gl As OpenGL, mesh As Mesh)
+        For Each prim In mesh.Primitives
+            RenderPrimitive(gl, prim)
+        Next
+    End Sub
+
+    Private Sub RenderPrimitive(gl As OpenGL, prim As MeshPrimitive)
+        Dim positions = prim.VertexAccessors("POSITION").AsVector3Array()
+        Dim indices = prim.IndexAccessor.AsIndicesArray()
+        Dim normals = prim.VertexAccessors("NORMAL").AsVector3Array()
+        Dim texCoords = If(prim.VertexAccessors.ContainsKey("TEXCOORD_0"), prim.VertexAccessors("TEXCOORD_0").AsVector2Array(), Nothing)
+
+        If prim.Material IsNot Nothing Then
+            Dim baseColorChannel = prim.Material.FindChannel("BaseColor")
+            If baseColorChannel.HasValue Then
+                If baseColorChannel.Value.Texture IsNot Nothing Then
+                    Dim textureIndex = baseColorChannel.Value.Texture.LogicalIndex
+                    If textures.ContainsKey(textureIndex) Then
+                        gl.BindTexture(OpenGL.GL_TEXTURE_2D, textures(textureIndex))
+                    Else
+                        gl.BindTexture(OpenGL.GL_TEXTURE_2D, 0)
+                    End If
+                Else
+                    Dim baseColor = baseColorChannel.Value.Color
+                    gl.Color(baseColor.X, baseColor.Y, baseColor.Z, baseColor.W)
+                End If
+            End If
+        End If
+
+        gl.Begin(OpenGL.GL_TRIANGLES)
+        For Each index In indices
+            If texCoords IsNot Nothing Then
+                Dim texCoord = texCoords(index)
+                gl.TexCoord(texCoord.X, texCoord.Y)
+            End If
+            Dim normal = normals(index)
+            gl.Normal(normal.X, normal.Y, normal.Z)
+            Dim position = positions(index)
+            gl.Vertex(position.X, position.Y, position.Z)
+        Next
+        gl.End()
+    End Sub
+
+    Private Function TransformMatrix(matrix As Matrix4x4) As Double()
+        Return New Double() {
+            matrix.M11, matrix.M12, matrix.M13, matrix.M14,
+            matrix.M21, matrix.M22, matrix.M23, matrix.M24,
+            matrix.M31, matrix.M32, matrix.M33, matrix.M34,
+            matrix.M41, matrix.M42, matrix.M43, matrix.M44
+        }
+    End Function
+
+    Private Sub OpenglControl1_OpenGLDraw(sender As Object, args As RenderEventArgs) Handles OpenglControl1.OpenGLDraw
+        Dim gl As OpenGL = OpenglControl1.OpenGL
+
+        gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT Or OpenGL.GL_DEPTH_BUFFER_BIT)
+        gl.LoadIdentity()
+
+        Dim cameraX As Single
+        Dim cameraY As Single
+        Dim cameraZ As Single
+
+        If Single.TryParse(TextBox1.Text, cameraX) AndAlso Single.TryParse(TextBox2.Text, cameraY) AndAlso Single.TryParse(TextBox3.Text, cameraZ) Then
+            gl.LookAt(cameraX, cameraY, cameraZ, 0, 0, 0, 0, 1, 0)
+            RenderGLTFModel(gl)
+            gl.Flush()
+        End If
+    End Sub
+
+    Private Sub OpenglControl1_OpenGLInitialized(sender As Object, e As EventArgs) Handles OpenglControl1.OpenGLInitialized
+        Dim gl As OpenGL = OpenglControl1.OpenGL
+        gl.Enable(OpenGL.GL_TEXTURE_2D)
+        gl.Enable(OpenGL.GL_DEPTH_TEST)
+        gl.ShadeModel(OpenGL.GL_SMOOTH)
+        gl.PolygonMode(OpenGL.GL_FRONT_AND_BACK, OpenGL.GL_FILL)
+
+        If enableAntialiasing Then
+            gl.Enable(OpenGL.GL_POLYGON_SMOOTH)
+            gl.Enable(OpenGL.GL_BLEND)
+            gl.BlendFunc(OpenGL.GL_SRC_ALPHA, OpenGL.GL_ONE_MINUS_SRC_ALPHA)
+        End If
+    End Sub
+
+    Private Sub OpenglControl1_Resized(sender As Object, e As EventArgs) Handles OpenglControl1.Resized
+        Dim gl As OpenGL = OpenglControl1.OpenGL
+
+        ' Set the projection matrix
+        gl.MatrixMode(OpenGL.GL_PROJECTION)
+        gl.LoadIdentity()
+        gl.Perspective(45.0F, CSng(OpenglControl1.Width) / CSng(OpenglControl1.Height), 0.1F, 100.0F)
+
+        ' Back to modelview
+        gl.MatrixMode(OpenGL.GL_MODELVIEW)
+        gl.LoadIdentity()
+    End Sub
+End Class
